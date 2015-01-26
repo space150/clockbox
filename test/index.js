e19474779dce32efae60a6dffd672effaca639d1
Mike Anderson <hello@mrmikea.com>
2015-01-26 16:34:39 -0600

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,19 @@
 var assert = require("assert"),
-    Calendar = require("..");
+    ClockBox = require("../lib");
 
 
-describe("Calendar", function() {
+describe("ClockBox", function() {
 
-  describe("#new", function() {
-    it("should have the proper number of dates", function() {
-      var c = new Calendar("3/17/2015", "3/18/2015");
+  describe("#getDayCount", function() {
+    it("should report the proper number of dates", function() {
+      var c = new ClockBox("3/17/2015", "3/18/2015");
       assert(c.getDayCount() === 2);
     });
   });
 
   describe("#store, #fetch", function() {
     it("should store data and allow it to be retrieved", function() {
-      var c = new Calendar("3/17/2015", "3/19/2015"),
+      var c = new ClockBox("3/17/2015", "3/19/2015"),
           date = new Date("2015-03-18T23:12:11.627Z");
       c.store(date, "wut");
       assert(c.fetch(date) === "wut");
