e834f39a63922201384ad5add0d0a304c7f88c5a
Mike Anderson <hello@mrmikea.com>
2015-01-26 17:57:40 -0600

    Add #clear

--- test/index.js
+++ test/index.js
@@ -14,9 +14,19 @@ describe("ClockBox", function() {
   describe("#store, #fetch", function() {
     it("should store data and allow it to be retrieved", function() {
       var c = new ClockBox("3/17/2015", "3/19/2015"),
-          date = new Date("2015-03-18T23:12:11.627Z");
+          date = new Date("2015-03-18T14:00:00Z");
       c.store(date, "wut");
       assert(c.fetch(date) === "wut");
+    });
+  });
+
+  describe("#clear", function() {
+    it("clear data for the given hour", function() {
+      var c = new ClockBox("3/17/2015", "3/19/2015"),
+          date = new Date("2015-03-18T14:00:00Z");
+      c.store(date, "wut");
+      c.clear(date);
+      assert(c.fetch(date) === null);
     });
   });
 
